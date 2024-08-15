e4e8f8aec8bddfd039958af9a8c3c119529f5ad7
ROJAN ROY <87638029+Rojan21@users.noreply.github.com>
2024-08-15 02:52:21 +0000

    created category list

--- src/EditCategoryModal.jsx
+++ src/EditCategoryModal.jsx
@@ -1,10 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 import './EditModal.css'
+import { useSelector } from "react-redux";
 
 
 const EditCategoryModal = forwardRef((props,ref)=>{
     
-    
+    const data = useSelector((state)=>state.widget.categories)
     
     
     const dialogref = useRef();
@@ -29,14 +30,49 @@ const EditCategoryModal = forwardRef((props,ref)=>{
             <h1>Customize your DashBoard</h1>
             <div className="Edit_div_main">
                 <div className="edit_categories">
+                    
+                    <div className="categorie_head">
                     <h3>Categories</h3>
-                    <div>kdkfkdkdfd</div>
+                        </div> 
+                    
+                    <div className="category_list">
+
+                    {data.map((item)=><div>{item.categoryName}</div>)}
+                    </div>
                     
 
 
                 </div>
                 <div >
-                    <div className="edit_widget">Widgets</div>
+                    <div className="edit_widget">
+
+                    <div className="widget_head">
+                    <h3>Widgets</h3>
+                        </div> 
+                    
+                    <div className="widget_list">
+                        <div className="widget_list_div">
+                            <div>Widget1</div>
+                            <div>Widget2</div>
+                        </div>
+                        <div className="widget_list_div">
+                            <div>Widget1</div>
+                            <div>Widget2</div>
+                        </div>
+                        <div className="widget_list_div">
+                            <div>Widget1</div>
+                            <div>Widget2</div>
+                        </div>
+                        <div className="widget_list_div">
+                            <div>Widget1</div>
+                            <div>Widget2</div>
+                        </div>
+
+                   
+                    </div>
+
+
+                    </div>
                     <div className="edit_buttons">
                         <div className="edit-button-confirm">
 
